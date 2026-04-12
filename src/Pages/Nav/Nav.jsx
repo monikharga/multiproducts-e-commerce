a8e5bbc38e53fb814b061e054d89bbf46437be8d
Moni Kharga <monikharga@gmail.com>
2026-04-13 01:10:18 +0530

    adding menubar

--- src/Pages/Nav/Nav.jsx
+++ src/Pages/Nav/Nav.jsx
@@ -2,22 +2,55 @@ import Bird from './bird.jpg'
 import './Nav.css'
 import SignUp from '../signUp/Sign'
 import { useNavigate } from 'react-router-dom'
-const Nav=()=>{
- const navigate=useNavigate()
+import menu from './menubar1.png'
+import cross from './cross-23.png'
+import { useState } from 'react'
+const Nav = () => {
+    const navigate = useNavigate()
+    const [menuu, setmenu] = useState(true)
+
+    const handleMenu = () => {
+        
+            setmenu(false)
+        
+    }
+    const handlecross = () => {
+       
+            setmenu(true)
+        
+    }
+    console.log(menuu)
     return (
         <>
-        <div className='NavBar'>
-            <div>
-                <img src={Bird} alt="image" className='birdImg'/>
+            <div className='NavBar'>
+                <div>
+                    <img src={Bird} alt="image" className='birdImg' />
+                </div>
+                <div id='links'>
+                   <div id='link' className={menuu ? "" : "active"}>
+                        <a className='a' href="" target="_blank">Home</a>
+                        <a className='a' href="">Product</a>
+                        <a className='a' href="">About</a>
+                        <a className='a' href="">Contact</a>
+                    </div>
+                    <div>
+                        <button id='button' onClick={() => navigate('/Signup')} >Sign Up</button>
+                    </div>
+                    <div>
+                        {
+                            menuu ?
+                                <img className='menu pad' src={menu} alt='img' onClick={handleMenu}
+
+                                ></img>
+                                :
+                                <img className='cross pad' src={cross} onClick={handlecross}></img>
+
+                        }
+
+                    </div>
+                </div>
+
             </div>
-            <div id='links'>
-                <a className='a' href="" target="_blank">Home</a>
-                <a className='a' href="">Product</a>
-                <a className='a' href="">About</a>
-                <a className='a' href="">Contact</a>
-                <button id='button'onClick={()=>navigate('/Signup')} >Sign Up</button>
-            </div>
-        </div>
         </>
     )
 
